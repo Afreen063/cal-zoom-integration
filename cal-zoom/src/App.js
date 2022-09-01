c10a0ba5e1c20eff427362f56a5fe30b7a460ae0
Afreen063 <afreen0501@gmail.com>
2022-09-01 22:28:20 +0530

    a

--- cal-zoom/src/App.js
+++ cal-zoom/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { InlineWidget } from "react-calendly";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InlineWidget url="https://calendly.com/your_scheduling_page" />
     </div>
   );
-}
+};
 
 export default App;
