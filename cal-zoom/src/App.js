eb8698e92ef4d2180082ee6d39525a94c2bcf902
Afreen063 <afreen0501@gmail.com>
2022-09-21 18:16:35 +0530

    link calendly

--- cal-zoom/src/App.js
+++ cal-zoom/src/App.js
@@ -1,21 +1,13 @@
 
-import { useState } from "react";
-
+import React, { useState } from "react";
+import { InlineWidget } from "react-calendly";
 
 const App = () => {
-  const [open, setOpen] =useState("");
-  function handleClick()
-  {
-    fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=9904b4a740bf476383cc75a848f17e0d")
-    .then(x => x.json())
-    .then(y => console.log(y.articles[0].author ))
-  }
+  const [value, setvalue]=useState(false);
   return (
     <div className="App">
-      <button  style={{display:"block",margin:"0 auto"}} 
-      onClick={handleClick}
-      >Click it</button>
-     
+      <button onClick={()=>{setvalue(true)}}>click me</button>
+      {value?<InlineWidget url="https://calendly.com/afreen0501" />:null}
     </div>
   );
 };
